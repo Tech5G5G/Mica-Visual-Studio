89ace6532ccd20c00fc8b628231ad87476b5874c
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-03-17 19:08:01 -0700

    Add WPF options page

---
+++ MicaVisualStudio/Options/GeneralPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Community.VisualStudio.Toolkit;
+
+namespace MicaVisualStudio.Options
+{
+    /// <summary>
+    /// Interaction logic for GeneralWpf.xaml
+    /// </summary>
+    public partial class GeneralPage : UserControl
+    {
+        public GeneralPage()
+        {
+            InitializeComponent();
+
+            var general = General.Instance;
+
+            InitializeComboBox(backdrop, general, nameof(General.Backdrop));
+            InitializeComboBox(theme, general, nameof(General.Theme));
+            InitializeComboBox(cornerPreference, general, nameof(General.CornerPreference));
+
+            InitializeComboBox(toolBackdrop, general, nameof(General.ToolBackdrop)); 
+            InitializeComboBox(toolTheme, general, nameof(General.ToolTheme));
+            InitializeComboBox(toolCornerPreference, general, nameof(General.ToolCornerPreference));
+
+            toolWindowsGrid.Visibility = (toolWindows.IsChecked = general.ToolWindows) == true ? Visibility.Visible : Visibility.Collapsed;
+            toolWindows.Click += (s, e) =>
+            {
+                toolWindowsGrid.Visibility = (General.Instance.ToolWindows = toolWindows.IsChecked == true) ? Visibility.Visible : Visibility.Collapsed;
+                General.Instance.Save();    
+            };
+        }
+
+        private void InitializeComboBox<T>(ComboBox box, T model, string propertyName) where T : BaseOptionModel<T>, new()
+        {
+            var prop = model.GetType().GetProperty(propertyName);
+            box.SelectedIndex = (int)prop.GetValue(model);
+            box.SelectionChanged += (s, e) =>
+            {
+                prop.SetValue(General.Instance, box.SelectedIndex);
+                General.Instance.Save();
+            };
+        }
+    }
+}

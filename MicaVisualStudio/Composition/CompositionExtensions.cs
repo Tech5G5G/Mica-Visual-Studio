18ec3e7b910dbe5047cd9350bf833c61a06375a2
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-06-22 16:09:32 -0700

    Add CompositionExtensions.cs

---
+++ MicaVisualStudio/Composition/CompositionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MicaVisualStudio.Composition;
+
+public static class CompositionExtensions
+{
+    public static void CreateDesktopWindowTarget(this Compositor compositor, nint hWnd, bool isTopmost, out DesktopWindowTarget target)
+    {
+        var interop = (ICompositorDesktopInterop)(object)compositor;
+        interop.CreateDesktopWindowTarget(hWnd, isTopmost, out target);
+    }
+
+    [ComImport]
+    [Guid("29E691FA-4567-4DCA-B319-D0F207EB6807")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    private interface ICompositorDesktopInterop
+    {
+        void CreateDesktopWindowTarget(IntPtr hwndTarget, bool isTopmost, out DesktopWindowTarget target);
+    }
+}

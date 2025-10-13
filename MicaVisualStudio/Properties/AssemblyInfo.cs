d29b17cb495d63849de491754280fa3c2936d89c
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-10-12 18:47:14 -0700

    Update assembly info

--- MicaVisualStudio/Properties/AssemblyInfo.cs
+++ MicaVisualStudio/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicaVisualStudio")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("Mica Visual Studio")]
+[assembly: AssemblyCopyright("© 2025 Ignacy Tomczyk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

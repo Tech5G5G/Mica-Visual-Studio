e671ccffd846ab552946f6bdba9fe5fd7f73c6bf
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-13 19:59:45 -0800

    Add TreeExtensions

---
+++ MicaVisualStudio/Extensions/TreeExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace MicaVisualStudio.Extensions;
+
+public static class TreeExtensions
+{
+    public static IEnumerable<DependencyObject> LogicalDescendants(this DependencyObject parent)
+    {
+        foreach (var child in LogicalTreeHelper.GetChildren(parent).OfType<DependencyObject>())
+        {
+            yield return child;
+
+            foreach (var obj in LogicalDescendants(child))
+                yield return obj;
+        }
+    }
+
+    public static IEnumerable<T> LogicalDescendants<T>(this DependencyObject parent) where T : DependencyObject =>
+        LogicalDescendants(parent).OfType<T>();
+
+    public static T FindElement<T>(this IEnumerable<FrameworkElement> source, string name) where T : FrameworkElement =>
+        source.FirstOrDefault(i => i is T element && element.Name == name) as T;
+}

94a74a718d31491834256c407ccd4a64286167d0
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-13 20:00:18 -0800

    Add EnumFramesExtensions

---
+++ MicaVisualStudio/VisualStudio/EnumFramesExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MicaVisualStudio.VisualStudio;
+
+public static class EnumFramesExtensions
+{
+    public static IEnumerable<IVsWindowFrame> ToEnumerable(this IEnumWindowFrames @enum)
+    {
+        ThreadHelper.ThrowIfNotOnUIThread();
+
+        const uint Amount = 1;
+        var frames = new IVsWindowFrame[Amount];
+
+        while (ErrorHandler.Succeeded(@enum.Next(Amount, frames, out uint fetched)) && fetched == Amount)
+            yield return frames[0];
+    }
+}

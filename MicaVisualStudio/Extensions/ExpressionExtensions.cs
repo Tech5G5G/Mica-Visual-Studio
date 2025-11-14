1422bd20b19d0f7095a066f3024fae45cd900ac3
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-13 19:59:25 -0800

    Add ExpressionExtensions

---
+++ MicaVisualStudio/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Linq.Expressions;
+using Expression = System.Linq.Expressions.Expression;
+
+namespace MicaVisualStudio.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static UnaryExpression Convert(this Expression expression, Type type) =>
+            Expression.Convert(expression, type);
+
+        public static UnaryExpression Convert<T>(this Expression expression) =>
+            expression.Convert(typeof(T));
+
+        public static MemberExpression Property(this Expression expression, PropertyInfo property) =>
+            Expression.Property(expression, property);
+
+        public static Func<T, TResult> Compile<T, TResult>(this Expression body, params ParameterExpression[] parameters) =>
+            Expression.Lambda<Func<T, TResult>>(body, parameters).Compile();
+    }
+}

d40ddaa05500eb91641d582fcc3f6a99f50918b9
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-15 18:05:21 -0800

    Various changes

--- MicaVisualStudio/MicaVisualStudioPackage.cs
+++ MicaVisualStudio/MicaVisualStudioPackage.cs
@@ -1,84 +1,76 @@
 ﻿namespace MicaVisualStudio;
 
+/// <summary>
+/// This is the class that implements the package exposed by this assembly.
+/// </summary>
+/// <remarks>
+/// <para>
+/// The minimum requirement for a class to be considered a valid package for Visual Studio
+/// is to implement the IVsPackage interface and register itself with the shell.
+/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+/// to do it: it derives from the Package class that provides the implementation of the
+/// IVsPackage interface and uses the registration attributes defined in the framework to
+/// register itself and its components with the shell. These attributes tell the pkgdef creation
+/// utility what data to put into .pkgdef file.
+/// </para>
+/// <para>
+/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+/// </para>
+/// </remarks>
+[PackageRegistration(AllowsBackgroundLoading = true, UseManagedResourcesOnly = true)]
+[Guid(PackageGuidString)]
+[ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "Mica Visual Studio", "General", 0, 0, true, SupportsProfiles = true)]
+[ProvideProfile(typeof(OptionsProvider.GeneralOptions), "Mica Visual Studio", "General", 0, 0, true)]
+[ProvideOptionPage(typeof(OptionsProvider.ToolOptions), "Mica Visual Studio", "\u200BTool Windows", 0, 0, true, SupportsProfiles = true)]
+[ProvideProfile(typeof(OptionsProvider.ToolOptions), "Mica Visual Studio", "Tool Windows", 0, 0, true)]
+[ProvideOptionPage(typeof(OptionsProvider.DialogOptions), "Mica Visual Studio", "Dialog Windows", 0, 0, true, SupportsProfiles = true)]
+[ProvideProfile(typeof(OptionsProvider.DialogOptions), "Mica Visual Studio", "Dialog Windows", 0, 0, true)]
+[ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+[ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
+[ProvideAutoLoad(UIContextGuids.EmptySolution, PackageAutoLoadFlags.BackgroundLoad)]
+public sealed class MicaVisualStudioPackage : AsyncPackage
+{
     /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
+    /// MicaVisualStudioPackage GUID string.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
-    [PackageRegistration(AllowsBackgroundLoading = true, UseManagedResourcesOnly = true)]
-    [Guid(PackageGuidString)]
-    [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "Mica Visual Studio", "General", 0, 0, true, SupportsProfiles = true)]
-    [ProvideProfile(typeof(OptionsProvider.GeneralOptions), "Mica Visual Studio", "General", 0, 0, true)]
-    [ProvideOptionPage(typeof(OptionsProvider.ToolOptions), "Mica Visual Studio", "\u200BTool Windows", 0, 0, true, SupportsProfiles = true)]
-    [ProvideProfile(typeof(OptionsProvider.ToolOptions), "Mica Visual Studio", "Tool Windows", 0, 0, true)]
-    [ProvideOptionPage(typeof(OptionsProvider.DialogOptions), "Mica Visual Studio", "Dialog Windows", 0, 0, true, SupportsProfiles = true)]
-    [ProvideProfile(typeof(OptionsProvider.DialogOptions), "Mica Visual Studio", "Dialog Windows", 0, 0, true)]
-    [ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids.EmptySolution, PackageAutoLoadFlags.BackgroundLoad)]
-    public sealed class MicaVisualStudioPackage : AsyncPackage
-    {
-        /// <summary>
-        /// MicaVisualStudioPackage GUID string.
-        /// </summary>
-        public const string PackageGuidString = "1a10bdf6-6cb0-415e-8ddd-f16d897f1e4a";
+    public const string PackageGuidString = "1a10bdf6-6cb0-415e-8ddd-f16d897f1e4a";
 
-        #region Package Members
+    #region Package Members
 
     private ThemeHelper theme;
     private WindowManager manager;
 
-        private VsColorManager colors;
+    private VsColorManager colors;
     private VsWindowStyler styler;
 
     private (string Content, ImageMoniker Image) queuedInfo;
 
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-        /// <param name="progress">A provider for progress updates.</param>
-        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+    /// <summary>
+    /// Initialization of the package; this method is called right after the package is sited, so this is the place
+    /// where you can put all the initialization code that rely on services provided by VisualStudio.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+    /// <param name="progress">A provider for progress updates.</param>
+    /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+    protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+    {
+        await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-            try
-            {
+        try
+        {
             WindowManager.MainWindow.Loaded += Window_Loaded;
 
-                if (Environment.OSVersion.Version.Build < 22000) //Allow Windows 11 or later
-                {
-                    queuedInfo = ("Mica Visual Studio is not compatible with Windows 10 and earlier.", KnownMonikers.StatusWarning);
-                    return;
-                }
+            if (Environment.OSVersion.Version.Build < 22000) //Allow Windows 11 or later
+            {
+                queuedInfo = ("Mica Visual Studio is not compatible with Windows 10 and earlier.", KnownMonikers.StatusWarning);
+                return;
+            }
 
-            theme = ThemeHelper.Instance;
-            windows = WindowManager.Instance;
-
-                helper = new();
-                manager = VsWindowManager.Instance;
-                RefreshPreferences();
-
-                if (WindowManager.CurrentWindow is Window window) //Apply to start window
             colors = new()
 
             #region Resource Keys
 
-                {
+            {
                 ColorConfigs =
                 {
                     { "Background", new(translucent: true) },
@@ -142,34 +134,34 @@
             colors.VisualStudioThemeChanged += (s, e) => RefreshPreferences();
             theme.SystemThemeChanged += (s, e) => RefreshPreferences();
 
-                General.Saved += (s) => RefreshPreferences();
-            }
-            catch (Exception ex)
-            {
+            General.Saved += (s) => RefreshPreferences();
+        }
+        catch (Exception ex)
+        {
 #if DEBUG
-                Debug.WriteLine($"Error initializing Mica Visual Studio: {ex.Message}");
+            Debug.WriteLine($"Error initializing Mica Visual Studio: {ex.Message}");
 #endif
 
-                progress.Report(new("Mica Visual Studio", $"Error while initializing Mica Visual Studio:\n{ex.Message}"));
-                queuedInfo = ($"Error while initializing Mica Visual Studio: {ex.Message} ({ex.GetType().Name})\n{ex.StackTrace}", KnownMonikers.StatusError);
-            }
+            progress.Report(new("Mica Visual Studio", $"Error while initializing Mica Visual Studio:\n{ex.Message}"));
+            queuedInfo = ($"Error while initializing Mica Visual Studio: {ex.Message} ({ex.GetType().Name})\n{ex.StackTrace}", KnownMonikers.StatusError);
+        }
 
-            void Window_Loaded(object sender, RoutedEventArgs args)
-            {
-                if (queuedInfo.Content is not null)
+        void Window_Loaded(object sender, RoutedEventArgs args)
+        {
+            if (queuedInfo.Content is not null)
                 VS.InfoBar.CreateAsync(new(queuedInfo.Content, queuedInfo.Image)).Result.TryShowInfoBarUIAsync().Forget();
 
-                WindowManager.MainWindow.Loaded -= Window_Loaded;
-            }
+            WindowManager.MainWindow.Loaded -= Window_Loaded;
+        }
 
-            void RefreshPreferences()
-            {
-                General general = General.Instance;
+        void RefreshPreferences()
+        {
+            General general = General.Instance;
             theme.SetAppTheme(EvaluateTheme(general.AppTheme));
 
             foreach (var entry in manager.Windows)
                 ApplyWindowPreferences(entry.Key, entry.Value.Window, entry.Value.Type, firstTime: false, general);
-            }
+        }
 
         void AddWindow(Window window, WindowType type)
         {
@@ -177,7 +169,7 @@
             manager.Windows.Add(window.GetHandle(), (type, window));
             ApplyWindowPreferences(handle, window, type);
         }
-        }
+    }
 
     private void ApplyWindowPreferences(
         IntPtr handle,
@@ -185,45 +177,45 @@
         WindowType type,
         bool firstTime = true,
         General general = null)
-        {
-            general ??= General.Instance;
+    {
+        general ??= General.Instance;
 
-            if (firstTime && //Remove caption buttons once
+        if (firstTime && //Remove caption buttons once
             window is not null &&
             HwndSource.FromHwnd(handle) is HwndSource source)
-            {
+        {
             WindowHelper.ExtendFrameIntoClientArea(handle);
-                source.CompositionTarget.BackgroundColor = Colors.Transparent;
+            source.CompositionTarget.BackgroundColor = Colors.Transparent;
 
-                //Don't remove caption buttons from windows that need them
+            //Don't remove caption buttons from windows that need them
             if (window.WindowStyle == WindowStyle.None || window is not DialogWindowBase)
-                    WindowHelper.RemoveCaptionButtons(source);
-            }
+                WindowHelper.RemoveCaptionButtons(source);
+        }
 
-            switch (type)
-            {
-                default:
-                case WindowType.Main:
+        switch (type)
+        {
+            default:
+            case WindowType.Main:
                 ApplyWindowAttributes(
                     general.Theme,
                     (CornerPreference)general.CornerPreference,
                     (BackdropType)general.Backdrop);
-                    break;
+                break;
 
-                case WindowType.Tool when general.ToolWindows:
+            case WindowType.Tool when general.ToolWindows:
                 ApplyWindowAttributes(
                     general.ToolTheme,
                     (CornerPreference)general.ToolCornerPreference,
                     (BackdropType)general.ToolBackdrop);
-                    break;
+                break;
 
-                case WindowType.Dialog when general.DialogWindows:
+            case WindowType.Dialog when general.DialogWindows:
                 ApplyWindowAttributes(
                     general.DialogTheme,
                     (CornerPreference)general.DialogCornerPreference,
                     (BackdropType)general.DialogBackdrop);
-                    break;
-            }
+                break;
+        }
 
         void ApplyWindowAttributes(int theme, CornerPreference corner, BackdropType backdrop)
         {
@@ -234,30 +226,30 @@
     }
 
     private Theme EvaluateTheme(int theme) => (Theme)theme switch
-            {
+    {
         Theme.VisualStudio => colors.VisualStudioTheme,
         Theme.System => this.theme.SystemTheme,
         _ => (Theme)theme
-            };
+    };
 
-        protected override void Dispose(bool disposing)
-        {
+    protected override void Dispose(bool disposing)
+    {
         theme?.Dispose();
         manager?.Dispose();
 
         styler?.Dispose();
 
-            if (disposing)
-            {
+        if (disposing)
+        {
             theme = null;
             manager = null;
 
-                colors = null;
+            colors = null;
             styler = null;
-            }
-
-            base.Dispose(disposing);
         }
 
-        #endregion
+        base.Dispose(disposing);
     }
+
+    #endregion
+}

674759a440229e922c00bde64adcaddb97af8d50
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-16 14:43:22 -0800

    Make VsColorManager singleton

--- MicaVisualStudio/VisualStudio/VsColorManager.cs
+++ MicaVisualStudio/VisualStudio/VsColorManager.cs
@@ -3,12 +3,14 @@ using Microsoft.Internal.VisualStudio.PlatformUI;
 
 namespace MicaVisualStudio.VisualStudio;
 
-public class VsColorManager
+public sealed class VsColorManager
 {
+    public static VsColorManager Instance { get; } = new();
+
     private static Color TransparentWhite = Colors.Transparent,
         TranslucentBlack = Color.FromArgb(0x01, 0x00, 0x00, 0x00); //Slight alpha to change icon foreground (basically invisible)
 
-    public Dictionary<string, ColorConfig> ColorConfigs => configs;
+    public ReadOnlyDictionary<string, ColorConfig> Configs => new(configs);
     private readonly Dictionary<string, ColorConfig> configs = [];
 
     #region Visual Studio Theme
@@ -18,7 +20,7 @@ public class VsColorManager
 
     public event EventHandler<Theme> VisualStudioThemeChanged;
 
-    private readonly static ThemeResourceKey MainWindowActiveCaptionKey =
+    private readonly ThemeResourceKey MainWindowActiveCaptionKey =
         new(category: new("624ed9c3-bdfd-41fa-96c3-7c824ea32e3d"), name: "MainWindowActiveCaption", ThemeResourceKeyType.BackgroundColor);
 
     private readonly IVsUIShell5 shell = VS.GetRequiredService<SVsUIShell, IVsUIShell5>();
@@ -39,7 +41,7 @@ public class VsColorManager
 
     #endregion
 
-    public VsColorManager()
+    private VsColorManager()
     {
         GetVSTheme(out vsTheme);
         (Application.Current.Resources.MergedDictionaries as INotifyCollectionChanged).CollectionChanged += (s, e) =>
@@ -51,32 +53,28 @@ public class VsColorManager
         };
     }
 
+    private ColorConfig GetConfig(object key)
+    {
+        if (key is ThemeResourceKey theme &&
+            (theme.KeyType == ThemeResourceKeyType.BackgroundColor || theme.KeyType == ThemeResourceKeyType.BackgroundBrush))
+            return configs.TryGetValue(theme.Name, out ColorConfig config) ? config : null;
+        else if (key is string str)
+            return configs.TryGetValue(str.Replace("VsBrush.", null).Replace("VsColor.", null), out ColorConfig config) ? config : null;
+        else
+            return null;
+    }
+
+    private Color DetermineColor(Color color, ColorConfig config) =>
+        !config.IsTranslucent || (config.TransparentOnGray && color.R == color.G && color.G == color.B) ?
+        (vsTheme == Theme.Light ? TransparentWhite : TranslucentBlack) :
+        Color.FromArgb(Math.Min(config.Opacity, color.A), color.R, color.G, color.B);
+
     public void UpdateColors()
     {
-        List<object> keys = [];
-
         foreach (var dictionary in Application.Current.Resources.MergedDictionaries.OfType<DeferredResourceDictionaryBase>())
             foreach (var obj in dictionary)
             {
                 DictionaryEntry entry = (DictionaryEntry)obj;
-
-                //
-
-                //if (entry.Value switch
-                //{
-                //    _ when entry.Value is SolidColorBrush solid => solid.Color,
-                //    _ when entry.Value is GradientBrush gradient => gradient.GradientStops[0].Color,
-                //    _ when entry.Value is Color color => color,
-                //    _ => default
-                //} is Color color1 && color1.R == 0x2E && color1.G == 0x2E && color1.B == 0x2E)
-                //if ((entry.Key is ThemeResourceKey key ? key.Name : entry.Key is string str ? str : string.Empty).Contains("SolidBackgroundFillTertiary"))
-                //keys.Add(entry.Key);
-                //if (entry.Value is Brush)
-                //    dictionary[entry.Key] = Brushes.Red;
-                //else if (entry.Value is Color)
-                //    dictionary[entry.Key] = Colors.Red;
-
-                //
 
                 if (GetConfig(entry.Key) is not ColorConfig config)
                     continue;
@@ -98,24 +96,37 @@ public class VsColorManager
             }
     }
 
-    private ColorConfig GetConfig(object key)
+    #region Configuration
+
+    public void AddConfig(string key, ColorConfig config)
     {
-        if (key is ThemeResourceKey theme &&
-            (theme.KeyType == ThemeResourceKeyType.BackgroundColor || theme.KeyType == ThemeResourceKeyType.BackgroundBrush))
-            return configs.TryGetValue(theme.Name, out ColorConfig config) ? config : null;
-        else if (key is string str)
-            return configs.TryGetValue(str.Replace("VsBrush.", null).Replace("VsColor.", null), out ColorConfig config) ? config : null;
+        if (configs.ContainsKey(key))
+            configs[key] = config;
         else
-            return null;
+            configs.Add(key, config);
     }
 
-    private Color DetermineColor(Color color, ColorConfig config) =>
-        !config.IsTranslucent || (config.TransparentOnGray && color.R == color.G && color.G == color.B) ?
-        (vsTheme == Theme.Light ? TransparentWhite : TranslucentBlack) :
-        Color.FromArgb(Math.Min(config.Opacity, color.A), color.R, color.G, color.B);
+    public void AddConfigs(Dictionary<string, ColorConfig> configs)
+    {
+        foreach (var config in configs)
+            AddConfig(config.Key, config.Value);
+    }
+
+    public void RemoveConfig(string key)
+    {
+        if (configs.ContainsKey(key))
+            configs.Remove(key);
+    }
+
+    public void RemoveConfigs(params string[] keys)
+    {
+        foreach (var key in keys)
+            RemoveConfig(key);
+    }
+
+    #endregion
 }
 
-//TODO: Change default of translucent to true?
 public class ColorConfig(bool transparentOnGray = true, bool translucent = false, byte opacity = 0x38)
 {
     public static ColorConfig Default = new();

38d2a447c88124ca395663bd28a57337ca110a66
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-23 21:21:36 -0800

    Remove Contains extension method

--- MicaVisualStudio/Extensions/WeakEventExtensions.cs
+++ MicaVisualStudio/Extensions/WeakEventExtensions.cs
@@ -54,7 +54,7 @@ public class PropertyChangeNotifier : DependencyObject, IDisposable
         BindingOperations.ClearBinding(this, ValueProperty);
 }
 
-public static class WeakReferenceExtensions
+public static class WeakEventExtensions
 {
     #region Events
 
@@ -95,42 +95,6 @@ public static class WeakReferenceExtensions
                 notifier.Dispose();
             }
         }
-    }
-
-    #endregion
-
-    #region Utilities
-
-    /// <summary>
-    /// Determines whether <paramref name="source"/> contains a <see cref="WeakReference{T}"/> to <paramref name="value"/>.
-    /// </summary>
-    /// <remarks>
-    /// If <paramref name="source"/> is a <see cref="List{T}"/>, also removes redundant references; that is, references that no longer reference anything.
-    /// </remarks>
-    /// <typeparam name="T">The type of elements in <paramref name="source"/> that are weakly-referenced.</typeparam>
-    /// <param name="source">A sequence in which to locate a weakly-referenced value.</param>
-    /// <param name="value">The weakly-referenced value to locate in the sequence.</param>
-    /// <returns>
-    /// <see langword="true"/> if <paramref name="source"/> contains a <see cref="WeakReference{T}"/> to <paramref name="value"/>. Otherwise, <see langword="false"/>.
-    /// </returns>
-    public static bool Contains<T>(this IEnumerable<WeakReference<T>> source, T value) where T : class
-    {
-        if (source is List<WeakReference<T>> list)
-        {
-            for (int i = list.Count - 1; i >= 0; --i)
-                if (!list[i].TryGetTarget(out T element))
-                    list.RemoveAt(i); //Remove redundant reference
-                else if (element == value)
-                    return true;
-        }
-        else
-        {
-            foreach (var weakElement in source)
-                if (weakElement.TryGetTarget(out T element) && element == value)
-                    return true;
-        }
-
-        return false;
     }
 
     #endregion

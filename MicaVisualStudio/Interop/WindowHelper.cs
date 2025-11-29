a1a65ed3747d9d3d2713122a620fbdac54401446
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2025-11-29 14:44:29 -0800

    Add methods for extended window styles

--- MicaVisualStudio/Interop/WindowHelper.cs
+++ MicaVisualStudio/Interop/WindowHelper.cs
@@ -272,7 +272,8 @@ public static class WindowHelper
     [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
     private static extern uint SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
-    private const int GWL_STYLE = -16;
+    private const int GWL_STYLE = -16,
+        GWL_EXSTYLE = -20;
 
     public static WindowType GetWindowType(Window window)
     {
@@ -287,8 +288,10 @@ public static class WindowHelper
     }
 
     public static WindowStyles GetWindowStyles(IntPtr hWnd) => (WindowStyles)GetWindowLong(hWnd, GWL_STYLE);
-
     public static void SetWindowStyles(IntPtr hWnd, WindowStyles styles) => SetWindowLong(hWnd, GWL_STYLE, (uint)styles);
+
+    public static WindowStylesEx GetExtendedWindowStyles(IntPtr hWnd) => (WindowStylesEx)GetWindowLong(hWnd, GWL_EXSTYLE);
+    public static void SetExtendedWindowStyles(IntPtr hWnd, WindowStylesEx styles) => SetWindowLong(hWnd, GWL_EXSTYLE, (uint)styles);
 
     #endregion
 
@@ -362,6 +365,37 @@ public enum WindowStyles : uint
     ThickFrame = 0x00040000,
     Visible = 0x10000000,
     VScroll = 0x00200000
+}
+
+public enum WindowStylesEx : uint
+{
+    AcceptFiles = 0x00000010,
+    AppWindow = 0x00040000,
+    ClientEdge = 0x00000200,
+    Composited = 0x02000000,
+    ContextHelp = 0x00000400,
+    ControlParent = 0x00010000,
+    DLGModalFrame = 0x00000001,
+    Layered = 0x00080000,
+    LayoutRTL = 0x00400000,
+    Left = 0x00000000,
+    LeftScrollBar = 0x00004000,
+    LTRReading = 0x00000000,
+    MDIChild = 0x00000040,
+    NoActivate = 0x08000000,
+    NoInheritLayout = 0x00100000,
+    NoParentNotify = 0x00000004,
+    NoRedirectionBitmap = 0x00200000,
+    OverlappedWindow = WindowEdge | ClientEdge,
+    PaletteWindow = WindowEdge | ToolWindow | TopMost,
+    Right = 0x00001000,
+    RightScrollBar = 0x00000000,
+    RTLReading = 0x00002000,
+    StaticEdge = 0x00020000,
+    ToolWindow = 0x00000080,
+    TopMost = 0x00000008,
+    Transparent = 0x00000020,
+    WindowEdge = 0x00000100
 }
 
 #endregion

5940dabfaeff85ebd4c544f447c4a492ecbd3774
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-01-01 13:06:50 -0800

    Add ColorExtensions

---
+++ MicaVisualStudio/Extensions/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MicaVisualStudio.VisualStudio;
+
+/// <summary>
+/// Contains extensions for dealing with <see cref="Color"/>s.
+/// </summary>
+public static class ColorExtensions
+{
+    /// <summary>
+    /// Determines whether the specified <paramref name="color"/> is a shade of gray.
+    /// </summary>
+    /// <param name="color">A <see cref="Color"/> to check the grayness of.</param>
+    /// <returns>
+    /// Whether or not the specified <paramref name="color"/>'s
+    /// <see cref="Color.R"/>, <see cref="Color.G"/>, and <see cref="Color.B"/> channels are equal.
+    /// </returns>
+    public static bool IsGray(this Color color) =>
+        color.R == color.G && color.G == color.B;
+}

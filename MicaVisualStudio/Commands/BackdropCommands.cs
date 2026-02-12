400e32399f029000b2491b550d30e59cc9a53d33
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-02-11 23:29:58 -0800

    Make commands DI

---
+++ MicaVisualStudio/Commands/BackdropCommands.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Community.VisualStudio.Toolkit;
+using Community.VisualStudio.Toolkit.DependencyInjection;
+using Community.VisualStudio.Toolkit.DependencyInjection.Core;
+using MicaVisualStudio.Interop;
+using MicaVisualStudio.Contracts;
+
+namespace MicaVisualStudio;
+
+public abstract class BackdropCommand(DIToolkitPackage package, IBackdropManager manager) : BaseDICommand(package)
+{
+    protected abstract BackdropType Backdrop { get; }
+
+    private readonly IBackdropManager _manager = manager;
+
+    protected async override Task ExecuteAsync(OleMenuCmdEventArgs args) =>
+        _manager.Backdrop = Backdrop;
+
+    protected override void BeforeQueryStatus(EventArgs args) =>
+        Command.Checked = _manager.Backdrop == Backdrop;
+}
+
+[Command(PackageIds.NoneCommandId)]
+public sealed class NoneCommand(DIToolkitPackage package, IBackdropManager manager) : BackdropCommand(package, manager)
+{
+    protected override BackdropType Backdrop => BackdropType.None;
+}
+
+[Command(PackageIds.MicaCommandId)]
+public sealed class MicaCommand(DIToolkitPackage package, IBackdropManager manager) : BackdropCommand(package, manager)
+{
+    protected override BackdropType Backdrop => BackdropType.Mica;
+}
+
+[Command(PackageIds.TabbedCommandId)]
+public sealed class TabbedCommand(DIToolkitPackage package, IBackdropManager manager) : BackdropCommand(package, manager)
+{
+    protected override BackdropType Backdrop => BackdropType.Tabbed;
+}
+
+[Command(PackageIds.AcrylicCommandId)]
+public sealed class AcrylicCommand(DIToolkitPackage package, IBackdropManager manager) : BackdropCommand(package, manager)
+{
+    protected override BackdropType Backdrop => BackdropType.Acrylic;
+}
+
+[Command(PackageIds.GlassCommandId)]
+public sealed class GlassCommand(DIToolkitPackage package, IBackdropManager manager) : BackdropCommand(package, manager)
+{
+    protected override BackdropType Backdrop => BackdropType.Glass;
+}
---
+++ MicaVisualStudio/Commands/MoreCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Community.VisualStudio.Toolkit;
+using Community.VisualStudio.Toolkit.DependencyInjection;
+using Community.VisualStudio.Toolkit.DependencyInjection.Core;
+
+namespace MicaVisualStudio;
+
+[Command(PackageIds.MoreCommandId)]
+public sealed class MoreCommand(DIToolkitPackage package) : BaseDICommand(package)
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs args)
+    {
+        await VS.Settings.OpenAsync<OptionsProvider.GeneralOptions>();
+    }
+}

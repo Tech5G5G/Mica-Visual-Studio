345f9517e9579f052273ec339c2b3b463e9eccca
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-02-12 20:19:16 -0800

    Move PInvoke

---
+++ MicaVisualStudio/Interop/PInvoke.UxTheme.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MicaVisualStudio.Interop;
+
+internal static partial class PInvoke
+{
+    [DllImport("uxtheme.dll", EntryPoint = "#135")]
+    private static extern int SetPreferredAppMode(PreferredAppMode preferredAppMode);
+
+    [DllImport("uxtheme.dll", EntryPoint = "#136")]
+    private static extern void FlushMenuThemes();
+
+    public static void SetAppMode(PreferredAppMode mode)
+    {
+        SetPreferredAppMode(mode);
+        FlushMenuThemes();
+    }
+}
+
+public enum PreferredAppMode
+{
+    ForceDark = 2,
+    ForceLight = 3
+}

935f7d7145f1804de0779925dc038c9e6151dc06
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-02-16 19:53:39 -0800

    Add Logger

---
+++ MicaVisualStudio/Contracts/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.Imaging.Interop;
+using System;
+
+namespace MicaVisualStudio.Contracts;
+
+public interface ILogger
+{
+    void Log(string message);
+    void Log(Exception exception);
+
+    void Output(string message);
+    void Output(Exception exception);
+
+    void InfoBar(string message, ImageMoniker image);
+    void InfoBar(Exception exception, ImageMoniker image);
+}
---
+++ MicaVisualStudio/Services/Logger.cs
@@ -0,0 +1,44 @@
+﻿using MicaVisualStudio.Contracts;
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace MicaVisualStudio.Services;
+
+public class Logger(IVsActivityLog log, IInfoBarService service) : ILogger
+{
+    private readonly IVsActivityLog _log = log;
+    private readonly IInfoBarService _service = service;
+
+    public void Log(string message)
+    {
+        Debug.WriteLine(message);
+        _log.LogEntry((uint)__ACTIVITYLOG_ENTRYTYPE.ALE_ERROR, Assembly.GetCallingAssembly().GetName().Name, message);
+    }
+
+    public void Log(Exception exception) =>
+        Log(FormatException(exception));
+
+    public void Output(string message)
+    {
+        ExceptionExtensions.Log(exception: null, $"[{Assembly.GetCallingAssembly().GetName().Name}] {message}");
+        Log(message);
+    }
+
+    public void Output(Exception exception) =>
+        Output(FormatException(exception));
+
+    public void InfoBar(string message, ImageMoniker image)
+    {
+        _service.EnqueueInfoBarModel(new(message, image));
+        Output(message);
+    }
+
+    public void InfoBar(Exception exception, ImageMoniker image) =>
+        InfoBar(FormatException(exception), image);
+
+    private string FormatException(Exception exception) =>
+        exception + Environment.NewLine + exception.StackTrace;
+}

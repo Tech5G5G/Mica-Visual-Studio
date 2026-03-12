bf5bffe2045218247a7edff5638c1fa383ce7636
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-03-12 13:20:33 -0700

    Add GetOwner method

---
+++ MicaVisualStudio/Interop/PInvoke.Lineage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace MicaVisualStudio.Interop;
+
+internal partial class PInvoke
+{
+    [DllImport("user32.dll")]
+    private static extern nint GetParent(nint hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool EnumChildWindows(nint hWndParent, EnumChildProc lpEnumFunc, nint lParam);
+
+    private delegate bool EnumChildProc(nint hwnd, nint lParam);
+
+    public static nint GetOwner(nint hWnd)
+    {
+        return GetParent(hWnd);
+    }
+
+    public static IEnumerable<nint> GetChildren(nint hWnd)
+    {
+        List<nint> handles = [];
+        EnumChildWindows(hWnd, Proc, IntPtr.Zero);
+        return handles;
+
+        bool Proc(nint hwnd, nint lParam)
+        {
+            handles.Add(hwnd);
+            return true;
+        }
+    }
+}

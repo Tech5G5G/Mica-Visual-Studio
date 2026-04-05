c4c99dbb81b0a987263ab911db71f8b283e6cb02
Ignacy Tomczyk <ignacy@tomczykowie.pl>
2026-04-05 14:51:34 -0700

    Check system theme before invoking event

--- MicaVisualStudio/Services/ThemeService.cs
+++ MicaVisualStudio/Services/ThemeService.cs
@@ -10,25 +10,31 @@ public sealed class ThemeService : IThemeService, IDisposable
     public event EventHandler<Theme> SystemThemeChanged;
 
     public Theme SystemTheme => _theme;
-    private Theme _theme = GetSystemTheme();
+    private Theme _theme;
 
     public ThemeService()
     {
+        GetSystemTheme(out _theme);
         SystemEvents.InvokeOnEventsThread(new Action(() => SystemEvents.UserPreferenceChanging += OnPreferenceChanging));
     }
 
-    private static Theme GetSystemTheme()
+    private bool GetSystemTheme(out Theme theme)
     {
-        return (int)Registry.GetValue(
+        theme = (int)Registry.GetValue(
             keyName: @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
             valueName: "AppsUseLightTheme",
             defaultValue: 0)
             == 1 /* TRUE */ ? Theme.Light : Theme.Dark;
+        return _theme != theme;
     }
 
     private void OnPreferenceChanging(object sender, UserPreferenceChangingEventArgs e)
     {
-        SystemThemeChanged?.Invoke(this, _theme = GetSystemTheme());
+        if (e.Category == UserPreferenceCategory.General &&
+            GetSystemTheme(out var theme))
+        {
+            SystemThemeChanged?.Invoke(this, _theme = theme);
+        }
     }
 
     #region Dispose
